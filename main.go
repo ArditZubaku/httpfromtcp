3b8ece92c4830c9b769dc25b27fffdec49c4e353
ArditZubaku <zubaku92@gmail.com>
2025-08-14 16:50:18 +0200

    Passing lines through channel after reading in separate goroutine

--- main.go
+++ main.go
@@ -9,39 +9,53 @@ import (
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Fatal("ERR: ", err)
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+
+				out <- str
+
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
+
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("error", "error", err)
 	}
 
-	defer f.Close()
-
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			log.Fatal("ERR: ", err)
-		}
-
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-
-			fmt.Printf("read: %s\n", string(str))
-
-			str = ""
-		}
-
-		str += string(data)
-	}
-
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", string(str))
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
