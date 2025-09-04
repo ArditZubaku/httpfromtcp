b8a792966c047e2a7119d6943f7e16f49d27d6ce
ArditZubaku <zubaku92@gmail.com>
2025-09-04 10:57:22 +0200

    Validate field-name tokens

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,14 +3,46 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
-type Headers map[string]string
+func isValidToken(str []byte) bool {
+	// Lowercase letters: a-z
+	// Uppercase letters: A-Z
+	// Digits: 0-9
+	// Special characters:
+	for _, ch := range str {
+		found := false
+		if (ch >= 'A' && ch <= 'Z') ||
+			(ch >= 'a' && ch <= 'z') ||
+			(ch >= '0' && ch <= '9') {
+			found = true
+			switch ch {
+			case
+				'#',
+				'$',
+				'%',
+				'&',
+				'\'',
+				'*',
+				'+',
+				'-',
+				'.',
+				'^',
+				'_',
+				'`',
+				'|',
+				'~':
+				found = true
+			}
 
-var crlf = []byte("\r\n")
+			if !found {
+				return false
+			}
+		}
+	}
 
-func NewHeaders() Headers {
-	return map[string]string{}
+	return true
 }
 
 func parseHeader(fieldLine []byte) (string, string, error) {
@@ -27,6 +59,26 @@ func parseHeader(fieldLine []byte) (string, string, error) {
 	}
 
 	return string(name), string(value), nil
+}
+
+type Headers struct {
+	headers map[string]string
+}
+
+var crlf = []byte("\r\n")
+
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
+}
+
+func (h Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h Headers) Set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
 }
 
 func (h Headers) Parse(data []byte) (int, bool, error) {
@@ -50,8 +102,12 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 			return 0, false, err
 		}
 
+		if !isValidToken([]byte(name)) {
+			return 0, false, fmt.Errorf("malformed header name")
+		}
+
 		read += idx + len(crlf)
-		h[name] = value
+		h.Set(name, value)
 	}
 
 	return read, done, nil
